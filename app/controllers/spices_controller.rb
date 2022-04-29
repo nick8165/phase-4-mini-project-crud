7d7a4e403ee857b3e451ac6bd07049251abac612
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-04-29 15:27:35 -0500

    commit tree babb9bc38511fee78bbf3873e6f4fe9987fe10f6

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,35 @@
+class SpicesController < ApplicationController
+
+    def index
+        spice = Spice.all 
+        render json: spice, status: :ok
+    end
+
+    def show
+        spice = Spice.find_by(id: params[:id])
+        render json: spice, status: :ok
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render :json spice, status: :accepted
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
